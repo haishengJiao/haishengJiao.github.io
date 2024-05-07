6ee1c37ae88463e27a5f6fe5c3f5d9523583fea0
v-jiaohaisheng <v-jiaohaisheng@360.cn>
2024-05-07 16:06:54 +0800

    test

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 import { useThemeStore, useLangStore } from '@/stores'
 import { getStorageLanguage } from '@/lang'
 import i18n from '@/lang'
@@ -9,8 +9,12 @@ import dashboard from './dashboard'
 import vue from './vue'
 import css from './css'
 
+console.log(import.meta.env.BASE_URL)
+
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
+  // history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',

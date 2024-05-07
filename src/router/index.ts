9db2146c799e8f34e4528ad139d459545d57d538
v-jiaohaisheng <v-jiaohaisheng@360.cn>
2024-05-07 16:10:50 +0800

    test

--- src/router/index.ts
+++ src/router/index.ts
@@ -13,8 +13,8 @@ console.log(import.meta.env.BASE_URL)
 
 const router = createRouter({
   // history: createWebHistory(import.meta.env.BASE_URL),
-  history: createWebHistory(),
-  // history: createWebHashHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory(),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',

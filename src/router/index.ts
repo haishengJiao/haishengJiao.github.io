749187a20a76c10ecf81c666aa45dc37429df1ad
v-jiaohaisheng <v-jiaohaisheng@360.cn>
2024-05-07 16:14:02 +0800

    test2

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,12 +9,12 @@ import dashboard from './dashboard'
 import vue from './vue'
 import css from './css'
 
-console.log(import.meta.env.BASE_URL)
+console.log(import.meta.env.BASE_URL, '2')
 
 const router = createRouter({
   // history: createWebHistory(import.meta.env.BASE_URL),
   // history: createWebHistory(),
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
